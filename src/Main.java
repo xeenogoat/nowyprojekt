cec1aea476af33fcc294b0b29649733ca96909bf
xeenogoat <07kamilos@gmail.com>
2024-09-11 14:42:02 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //losowanie liczb do tablicy 6 elementowej
+        /*
+        tablica musi mieć  zdefinowanyrozmiar,
+        którego potem nie może przechowywać typy proste i złożone
+
+         */
+        int[] tablicaLiczbLosowych = new int[6];
+        for (int i = 0; i < tablicaLiczbLosowych.length ; i++) {
+            tablicaLiczbLosowych[i] = (int) (Math.random()*100+1);
+
+        }
+
+        System.out.println("WYlosowana Liczba");
+        for (int element:tablicaLiczbLosowych) {
+
+            System.out.println(element+", ");
+        }
     }
 }
